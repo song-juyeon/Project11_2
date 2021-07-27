91970b8d88faec880e93213fc1fb3c8f4b58c574
SongJuyeon <80025101+song-juyeon@users.noreply.github.com>
2021-07-27 11:52:17 +0900

    Edit GalleryAdapter.java (Dir11)Commit

--- app/src/main/java/kr/hs/emirim/w2034/project11_2/GalleryAdapter.java
+++ app/src/main/java/kr/hs/emirim/w2034/project11_2/GalleryAdapter.java
@@ -4,15 +4,21 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Gallery;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GalleryAdapter extends BaseAdapter {
     Context context;
-    int[] posterIds = {R.drawable.movie1, R.drawable.movie2, R.drawable.movie3, R.drawable.movie4, R.drawable.movie5, R.drawable.movie6, R.drawable.movie7, R.drawable.movie8, R.drawable.movie9, R.drawable.movie10};
+    String[] movieName = {"기생충", "겟아웃", "조커", "라라랜드", " 뷰티인사이드", "겨울왕국2", "침입자", "크루엘라", "알라딘", "보헤미안 랩소디"};
+    int[] posterIds = {R.drawable.movie1, R.drawable.movie2, R.drawable.movie3, R.drawable.movie4, R.drawable.movie5, R.drawable.movie6,
+                        R.drawable.movie7, R.drawable.movie8, R.drawable.movie9, R.drawable.movie10};
+    ImageView imgvLarge;
 
-
-    public GalleryAdapter(Context context){
+    public GalleryAdapter(Context context, ImageView imgvLarge){
         this.context = context;
+        this.imgvLarge = imgvLarge;
     }
     @Override
     public int getCount() {
@@ -32,9 +38,23 @@ public class GalleryAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageView imgv = new ImageView(context);
-        imgv.setLayoutParams(new ViewGroup.LayoutParams(200,300));
+        imgv.setLayoutParams(new Gallery.LayoutParams(200,300));
         imgv.setImageResource(posterIds[position]);
         imgv.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        final  int pos = position;
+        imgv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgvLarge.setImageResource(posterIds[pos]);
+                Toast toast = new Toast(context);
+                View toastViwe = View.inflate(context, R.layout.toast1, null);
+                TextView textView = toastViwe.findViewById(R.id.text_title);
+                textView.setText(movieName[pos]);
+                toast.setView(toastViwe);
+                toast.show();
+            }
+        });
+
         return imgv;
     }
 }
--- app/src/main/java/kr/hs/emirim/w2034/project11_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2034/project11_2/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Gallery;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("갤러리 영화 포스터");
+        ImageView imgvLarge = findViewById(R.id.imgv_large);
         Gallery gallery = findViewById(R.id.gallery1);
-        GalleryAdapter adapter = new GalleryAdapter(this);
+        GalleryAdapter adapter = new GalleryAdapter(this, imgvLarge);
         gallery.setAdapter(adapter);
 
     }
